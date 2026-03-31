0278642f6ca90bfebc129c57399b460824e1e411
sakshi9146 <128205338+sakshi9146@users.noreply.github.com>
2026-03-31 09:56:10 +0530

    sa

---
+++ terraform/ec2.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    secret_key = "" 
+    access_key = ""
+    region = "us-east-1"
+
+}
+
+# blockname  "cloudproviername_resourcetype" "uniquename" { #arguments}
+
+
+resource  "aws_instance" "webserver" {
+    ami = var.this_ami
+    instance_type =  var.this_inst 
+    key_name = var.this_key
+    disable_api_termination =  var.this_api
+    vpc_security_group_ids = [var.this_sg]
+    count = var.this_count
+}
