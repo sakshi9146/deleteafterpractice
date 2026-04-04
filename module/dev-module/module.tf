d68c83424aafdcba5436c4e00367df55690b8ad6
sakshi <sakshi0103mp@gmail.com>
2026-04-04 16:08:26 +0530

    new

--- module/dev-module/module.tf
+++ module/dev-module/module.tf
@@ -1,18 +1,18 @@
 module "ec2" {
-     source = "/home/cloudshell-user/deleteafterpractice/module/resource/EC2"
+     source = "/home/anup/deleteafterpractice/tfb34/module/resource/EC2"
      this_ami = "ami-01b14b7ad41e17ba4"
      this_inst = "t3.micro"
-     this_key = "sample"
-     this_api = false    
+     this_key = "b34"
+     this_api = false
      #this_sg = "sg-033f2559c156114d8"
      subnetid = module.vpc.aws_subnet
      vpc_id = module.vpc.aws_vpc
-}
+ }
 
 
 module "vpc" {
-   source = "/home/cloudshell-user/deleteafterpractice/module/resource/EC2"
+   source = "/home/anup/deleteafterpractice/tfb34/module/resource/VPC"
    cidrbloc = "10.0.0.0/16"
    subnetbloc = "10.0.1.0/24"
 
-} 
+}
